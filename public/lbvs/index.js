f37ae16459320d3cb13daea1e0106e27e9e01f5e
Jacky Lee <JackyLeeHongJian@Gmail.com>
2020-04-21 20:11:39 +0800

    Added lbvs/index.html and lbvs/index.js

---
+++ public/lbvs/index.js
@@ -0,0 +1,26 @@
+$(() => {
+	const query_label = $('#query_label');
+	$('#submit').click(() => {
+		const file = $('#query').get(0).files[0];
+		if (file === undefined || file.size > 50000) {
+			query_label.tooltip('show');
+			return;
+		}
+		const reader = new FileReader();
+		reader.onload = () => {
+			$.post('job', {
+				query: reader.result,
+				filename: file.name.substr(0, 20),
+				score: $('#score').val(),
+				database: $('#database').val(),
+			}, (res) => {
+				if (res.error) {
+					query_label.tooltip('show');
+					return;
+				}
+				location.assign(`result/?id=${res}`);
+			});
+		};
+		reader.readAsText(file);
+	});
+});
