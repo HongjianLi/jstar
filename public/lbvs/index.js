183299f9ba10a5d59502da5b0239f1a724ee88ad
Jacky Lee <JackyLeeHongJian@Gmail.com>
2020-05-10 11:41:01 +0800

    Renamed query variables.

--- public/lbvs/index.js
+++ public/lbvs/index.js
@@ -1,27 +1,27 @@
 $(() => {
 	$('[data-toggle="tooltip"]').tooltip();
-	const query_label = $('#query_label');
+	const qryMolSdfLabel = $('#qryMolSdfLabel');
 	$('#submit').click(() => {
-		const file = $('#query').get(0).files[0];
-		if (file === undefined || file.size > 50000) {
-			query_label.tooltip('dispose').attr('title', ['Maximum 50KB', 'No file selected'][+!file]).tooltip('show');
+		const qryMolSdfFile = $('#qryMolSdf').get(0).files[0];
+		if (qryMolSdfFile === undefined || qryMolSdfFile.size > 50000) {
+			qryMolSdfLabel.tooltip('dispose').attr('title', ['Maximum 50KB', 'No file selected'][+!qryMolSdfFile]).tooltip('show');
 			return;
 		}
 		const reader = new FileReader();
-		reader.onload = () => {
+		reader.onload = (e) => {
 			$.post('job', {
-				query: reader.result,
-				name: file.name.substr(0, 20),
+				qryMolSdf: e.target.result,
+				filename: qryMolSdfFile.name.substr(0, 20), // A typical ZINC15 sdf filename has 20 characters, e.g. ZINC012345678901.sdf
 				database: $('#database').val(),
 				score: $('#score').val(),
 			}, (res) => {
 				if (res.error) {
-					query_label.tooltip('dispose').attr('title', res.error).tooltip('show');
+					qryMolSdfLabel.tooltip('dispose').attr('title', res.error).tooltip('show');
 					return;
 				}
 				location.assign(`result/?id=${res.id}`);
 			});
 		};
-		reader.readAsText(file);
+		reader.readAsText(qryMolSdfFile);
 	});
 });
