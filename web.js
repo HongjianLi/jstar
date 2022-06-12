5cafe162999db959a6a6f46f4fb6d334b566a216
Jacky Lee <JackyLeeHongJian@Gmail.com>
2022-06-12 14:26:04 +0800

    Use commander to parse program options.

--- web.js
+++ web.js
@@ -1,7 +1,10 @@
 #!/usr/bin/env node
 import assert from 'assert/strict';
+import os from 'os';
+import path from 'path';
 import fs from 'fs';
 import cluster from 'cluster';
+import { program } from 'commander';
 import mongodb from 'mongodb';
 import Fastify from 'fastify';
 import fastifyStatic from '@fastify/static';
@@ -9,10 +12,23 @@ import fastifyFormbody from '@fastify/formbody';
 //import fastifyFavicon from 'fastify-favicon';
 import * as uuid from 'uuid';
 import child_process from 'child_process';
-import path from 'path';
 import { fileURLToPath } from 'url';
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 import cpdb from './public/cpdb/cpdb.js';
+
+// Process program options.
+const options = program
+	.name('jstar')
+	.description('jstar, a modern website for hosting drug discovery services.')
+	.version('1.0.0')
+	.option('--databases <string>', 'Path to the compound database directory', '../jstar/databases')
+	.option('--host <string>', 'DBMS host', 'localhost')
+	.option('--port <number>', 'DBMS port', cur => parseInt(cur), 27017)
+	.option('--user <string>', 'DBMS user', 'jstard')
+	.option('--pass <string>', 'DBMS password')
+	.option('--processes <number>', 'number of worker processes to use', cur => parseInt(cur), os.cpus().length)
+	.parse()
+	.opts();
 
 if (cluster.isPrimary) {
 	const descriptors = ['natm.u16', 'nhbd.u16', 'nhba.u16', 'nrtb.u16', 'nrng.u16', 'xmwt.f32', 'tpsa.f32', 'clgp.f32'].map((descriptor) => {
@@ -35,7 +51,7 @@ if (cluster.isPrimary) {
 	console.time('readFile');
 	await Promise.all(databases.map((db) => {
 		return Promise.all(db.descriptors.map((descriptor, index) => {
-			return fs.promises.readFile(`databases/${db.name}/${descriptor.file}`).then((buf) => {
+			return fs.promises.readFile(`${options.databases}/${db.name}/${descriptor.file}`).then((buf) => {
 				assert.equal(buf.length, descriptor.size * db.numCompounds);
 				descriptor.buf = buf;
 			}, console.error);
@@ -43,8 +59,7 @@ if (cluster.isPrimary) {
 	}));
 	console.timeEnd('readFile');
 	// Fork worker processes with cluster
-	const numWorkerProcesses = 4;
-	console.log('Forking %d worker processes', numWorkerProcesses);
+	console.log('Forking %d worker processes', options.processes);
 	function messageHandler (msg) { // Cannot use lambda function => because of 'this' binding. The 'this.send()' statement requires capturing the worker process.
 		if (msg.query === '/cpdb/count') {
 			const { s2m } = msg; // slave to master.
@@ -72,7 +87,7 @@ if (cluster.isPrimary) {
 			});
 		}
 	};
-	for (let i = 0; i < numWorkerProcesses; ++i) {
+	for (let i = 0; i < options.processes; ++i) {
 		cluster.fork().on('message', messageHandler); // Event 'message' occurs when the master receives a message from this specific worker.
 	}
 	cluster.on('exit', (worker, code, signal) => {
@@ -81,7 +96,7 @@ if (cluster.isPrimary) {
 	});
 } else {
 	// Connect to MongoDB
-	const mongoClient = new mongodb.MongoClient(`mongodb://jstard:2qR8dVM9d=@localhost:27017/?authSource=jstar&maxPoolSize=3`); // https://www.mongodb.com/docs/drivers/node/current/fundamentals/connection/ Always URI encode the username and password using the encodeURIComponent method to ensure they are correctly parsed.
+	const mongoClient = new mongodb.MongoClient(`mongodb://${options.user}:${options.pass}@${options.host}:${options.port}/?authSource=jstar&maxPoolSize=3`); // https://www.mongodb.com/docs/drivers/node/current/fundamentals/connection/ Always URI encode the username and password using the encodeURIComponent method to ensure they are correctly parsed.
 	await mongoClient.connect();
 	const jstar = mongoClient.db('jstar');
 	const lbvs = jstar.collection('lbvs');
@@ -103,7 +118,27 @@ if (cluster.isPrimary) {
 //	fastify.register(fastifyRecaptcha, { recaptcha_secret_key: 'your_recaptcha_sercret_key' }); // https://github.com/qwertyforce/fastify-recaptcha
 	// Define helper variables and functions
 	// Get the number of compounds satisfying filtering conditions
-	fastify.get('/cpdb/count', async (req, reply) => {
+	fastify.get('/cpdb/count', {
+		schema: {
+			querystring: { // body, querystring, params, and header
+				type: 'object',
+				properties: {
+					db: { type: 'string', enum: cpdb.map(cpdb => cpdb.name) },
+					descriptors: { type: 'object' },
+				},
+				required: [ 'db', 'descriptors' ],
+			},
+		},
+		response: {
+			200: {
+				type: 'object',
+				properties: {
+					numFilteredCompounds: { type: 'number' },
+					timestamp: { type: 'date' },
+				}
+			},
+		},
+	}, async (req, reply) => {
 		// Validate and sanitize user input
 /*		let v = new validator(req.query);
 		if (v
@@ -177,7 +212,7 @@ if (cluster.isPrimary) {
 				});
 			});
 			m2s.timestamp = Date.now();
-			return m2s;
+			reply.send(m2s);
 		});
 	});
 	fastify.get('/lbvs/job', {
@@ -238,7 +273,7 @@ if (cluster.isPrimary) {
 				properties: {
 					filename: { type: 'string', minLength: 1, maxLength: 20 },
 					qryMolSdf: { type: 'string', minLength: 1, maxLength: 500000 }, // Caution NoSQL injection
-					database: { type: 'string', enum: ['ZINC', 'SCUBIDOO', 'GDBMedChem', 'ChEMBL', 'ChemDiv', 'Specs', 'SuperNatural', 'SureChEMBL', 'COCONUT', 'Pfizer', 'NPASS', 'MedChemExpress', 'Selleckchem', 'DrugBank', 'GtoPdb', 'TargetMol', 'PADFrag', 'TTD', 'HybridMolDB', 'SWEETLEAD', 'SuperDRUG', 'Biopurify', 'EK-DRD', 'WITHDRAWN'] },
+					database: { type: 'string', enum: cpdb.map(cpdb => cpdb.name) },
 					score: { type: 'string', enum: ['USR', 'USRCAT'] },
 				},
 				required: [ 'filename', 'qryMolSdf', 'database', 'score' ],
